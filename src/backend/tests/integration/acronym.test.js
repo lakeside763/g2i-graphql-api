c1fe8860fb55599c705225e5abec2993d397ef5c
lakeside763 <lakeside763@gmail.com>
2021-07-07 14:21:35 +0100

    feat: update bump version

--- src/backend/tests/integration/acronym.test.js
+++ src/backend/tests/integration/acronym.test.js
@@ -68,7 +68,6 @@ describe('sanity check', () => {
       variables: { input: updateInput },
     });
     expect(statusCode).toBe(200);
-    expect(updateAcronym).toHaveProperty('id');
     expect(updateAcronym).toHaveProperty('acronym');
     expect(updateAcronym).toHaveProperty('meaning', updateInput.meaning);
   });
